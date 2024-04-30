cd7868db27f808a925e1480e6d28a1fbb7b144f2
Shohjahon03 <quvvatovshohjahon707@gmail.com>
2024-04-30 08:12:07 +0500

    add

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -3,10 +3,10 @@ import guvohnomas from '../../src/assets/guvohnoma.png';
 import chaq from '../../src/assets/chaq.png';
 import ear from '../../src/assets/earth.png';
 import komp from '../../src/assets/komp.png';
-import bay from '../../src/assets/bayoma.png';
+import bay from '../../src/assets/kelishuv.png';
 import lupa from '../../src/assets/lupa.png';
 import kash from '../../src/assets/kasholok.png';
-import { Accordion } from "flowbite-react";
+import { Accordion, Table } from "flowbite-react";
 const HomePage = () => {
 
   return (
@@ -96,11 +96,68 @@ const HomePage = () => {
     </Accordion>
            </div>
       </section>
-      <section className='pt-[50px] flex flex-col items-center pb-[50px]'>
+      <section className='pt-[50px] flex flex-col border-b  mb-[50px] items-center pb-[50px]'>
            <div className='container'>
            <p className='text-[40px] md:text-[60px] font-bold text-center tracking-6 text-blue-700 mb-[50px]'>Patentlash Narxlari</p>
            
+           <Table>
+        <Table.Head>
+          <Table.HeadCell>Olish muddati</Table.HeadCell>
+          <Table.HeadCell>Tezkor</Table.HeadCell>
+          <Table.HeadCell>Oddiy</Table.HeadCell>
+        </Table.Head>
+        <Table.Body className="divide-y">
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800 hover:bg-blue-400">
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+              {'Davlat boji'}
+            </Table.Cell>
+            <Table.Cell className='text-[10px] md:text-[15px]'>5 746 000 so'm</Table.Cell>
+            <Table.Cell className='text-[10px] md:text-[15px]'>
+             1 666 000 so'm
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800 hover:bg-blue-400">
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">
+             Ekspert tekshiruvi
+            </Table.Cell>
+            <Table.Cell className='text-[11px] md:text-[15px]'>750 000 so'm</Table.Cell>
+            <Table.Cell className='text-[10px] md:text-[15px]'>
+            750 000 so'm
+            </Table.Cell>
+          </Table.Row>
+          <Table.Row className="bg-white dark:border-gray-700 dark:bg-gray-800 hover:bg-blue-400">
+            <Table.Cell className="whitespace-nowrap font-medium text-gray-900 dark:text-white">Jami</Table.Cell>
+            <Table.Cell className='text-[10px] md:text-[15px]'>9 496 000 so'm</Table.Cell>
+            <Table.Cell className='text-[10px] md:text-[15px]'>
+             4 396 000 so'm
+            </Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+            <p className='text-center mt-[20px] text-blue-500'>Himoya qilish muddati 10 yil</p>
            </div>
+      </section>
+      <section>
+        <div className="container">
+          <div className='w-full'>
+                <p className='text-[40px] text-blue-600 font-semibold mb-[50px]'>Ishni qachon <br />boshlasak bo'ladi?</p>
+                <div className='flex flex-col items-center md:flex-row md:items-start justify-between flex-wrap'>
+                      <p className='text-[30px] text-center md:text-start'>Siz bilan hamkorlik <br />qilishdan mamnunmiz</p>
+                      <img className='w-[400px]' src={bay} alt="" />                  
+                </div>
+                <div className='flex justify-between items-center'>
+                    <div className='flex flex-col text-blue-600'>
+                      <a href=""><i class='bx bxs-phone'></i> 91 009-66-30</a>
+                      <a href=""><i class='bx bxl-telegram'></i> amaan.uz</a>
+                    </div>
+                    <div className=''>
+                      <p className='text-[40px] p-0 font-bold text-blue-600'>Amaan</p>
+                      <p className='text-blue-600'>patentlash agentligi</p>
+                    </div>
+                </div>
+            </div>       
+            
+         </div>
       </section>
     </main>
   )
